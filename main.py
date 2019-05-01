c875278dc85e5dd0f98894f4972ff7c793900069
buchelis <buchelis@edu.htl-villach.at>
2019-05-01 11:07:45 +0200

    implemented an movement of the block in the game frame

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ class Main(Tk):
         self.frame_game = Frame(self, height=self.height, width=self.width, bg='#484848')
         self.frame_block = Frame(self.frame_game, height=self.block_size, width=self.block_size, bg='#ff0000')
 
+        # bind the game inputs to the game frame
+        self.frame_game.bind('<a>', self.shift)
+        self.frame_game.bind('<d>', self.shift)
+        self.frame_game.bind('<s>', self.shift)
+        self.frame_game.focus()
+
         # place those frames
         self.frame_game.pack()
         self.frame_block.place(x=0, y=0)
@@ -28,13 +34,39 @@ class Main(Tk):
         # set the actual clock and save it
         self.after_id = self.after(self.clock_timer, self.clock)
 
-        # get the height of the block
-        height = self.frame_block.winfo_y()
+        # get the position of the block
+        block_x = self.frame_block.winfo_x()
+        block_y = self.frame_block.winfo_y()
 
         # replace the block if it didn't hit the bottom
-        if height < (self.height - self.block_size):
-            height += self.block_size
-            self.frame_block.place(x=0, y=height)
+        if block_y < (self.height - self.block_size):
+            block_y += self.block_size
+            self.frame_block.place(x=block_x, y=block_y)
+
+    def shift(self, event):
+        # get the position of the block
+        block_x = self.frame_block.winfo_x()
+        block_y = self.frame_block.winfo_y()
+
+        # return when the block is at the bottom
+        if block_y >= (self.height - self.block_size):
+            return
+
+        # set the new position
+        if event.keysym == 'a':
+            if block_x > 0:
+                block_x -= self.block_size
+
+        elif event.keysym == 'd':
+            if block_x < (self.width - self.block_size):
+                block_x += self.block_size
+
+        elif event.keysym == 's':
+            if block_y < (self.height - self.block_size):
+                block_y += self.block_size
+
+        # place the frame at the new position
+        self.frame_block.place(x=block_x, y=block_y)
 
 
 Main().mainloop()
